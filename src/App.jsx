7a016bdb1c5e61cc0439c242610e29c49eacff94
Cristian Ferreira <ferreira.8390@gmail.com>
2023-08-14 00:08:47 -0400

    alert socialbutton main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 
   return (
-    <>
+    <>    
 
     </>
   )
-
 
 export default App
---
+++ src/components/SocialButton.jsx
@@ -0,0 +1,12 @@
+
+
+const SocialButton = ({ icon }) => {
+    return
+    (
+        <>
+        <i className={icon}></i>
+        </>
+    )
+}
+
+export default SocialButton;
