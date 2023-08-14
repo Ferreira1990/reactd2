2a15f81a6b6b94598ceed124efd3c4a488fdfb0e
jpbenvenuto <jpbenvenu@gmail.com>
2023-08-14 00:38:17 -0400

    form4

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 
 import { useState } from 'react';
 import Registro from './components/Registro';
-import "./App.css";
+
 
 function App (){
   const [alert, setAlert] = useState ({msg:"", color:""});
